4b387a8ca6cb0e1f242372f410708e6c02a2523e
bondr007 <bondr@nsuok.edu>
2018-05-24 17:34:59 -0500

    Update email_stats.py

--- email_stats.py
+++ email_stats.py
@@ -7,16 +7,11 @@ import email
 import email.header
 import datetime
 
-import checkdmarc
 import re
 import json
-from pygments import highlight, lexers, formatters
-from pygments.formatters import Terminal256Formatter
-from pygments.styles import get_style_by_name
-from pygments.styles import STYLE_MAP
 
 
-EMAIL_ACCOUNT = "asdf@gmail.com"
+EMAIL_ACCOUNT = "dmarc@nsuok.edu"
 
 # Use 'INBOX' to read inbox.  Note that whatever folder is specified, 
 # after successfully running this script all emails in that folder 
@@ -87,10 +82,6 @@ def process_mailbox(M):
         cur_domain = mfrom.split("@")[1]
         cur_domain = cur_domain.replace(">", "")
 
-        # print('Message FROM: %s: ' % (mfrom))
-        # print('Message Domain: %s: ' % (str(cur_domain)))
-        # print('Message DKIM: %s: ' % (dkimhdr))
-
         if 'Received-SPF' in msg:
           spfhdr = email.header.make_header(email.header.decode_header(msg['Received-SPF']))
           spfhdr = str(spfhdr)
@@ -114,12 +105,10 @@ def process_mailbox(M):
             dkimSig = str(dkimhdr)
             if str("dkim") in arcAuthResults:
               report['dkim']['count'] += 1
-              #dkimCount += 1
               if str("dkim=pass") in arcAuthResults:
                 report['dkim']['pass'] += 1
               if str("dkim=pass") not in arcAuthResults:
                 report['dkim']['fail'] += 1
-                #dkimFailCount += 1
               if str("d=" + cur_domain) not in dkimSig:
                 report['dkim']['SenderAlignmentFailure'] += 1
 
@@ -127,12 +116,15 @@ def process_mailbox(M):
         if 'ARC-Authentication-Results' in msg:
           arc = email.header.make_header(email.header.decode_header(msg['ARC-Authentication-Results']))
           arcAuthResults = str(arc)
+          print(arcAuthResults)
+          print()
+          print()
           if str("dmarc") in arcAuthResults:
             report['dmarc']['count'] += 1
             if str("dmarc=pass") in arcAuthResults:
               report['dmarc']['pass'] += 1
             else:
-              report['dmarc']['reject'] += 1
+              report['dmarc']['fail'] += 1
 
             if str("p=REJECT") in arcAuthResults:
               report['dmarc']['policy']['reject'] += 1
@@ -160,8 +152,8 @@ def process_mailbox(M):
     print('Alignment Fail?:     %s   %.2f%%' % (report['dkim']['SenderAlignmentFailure'],percentage(report['dkim']['SenderAlignmentFailure'],report['dkim']['count']) ))
     print("")
     print('Total Using DMARC:   %s   %.2f%% of totalDomains are using DMARC' % (report['dmarc']['count'],percentage(report['dmarc']['count'],report['totalDomains']) ))
-    print('Total DMARC FAIL:    %s   %.2f%%' % (report['dkim']['fail'],percentage(report['dkim']['fail'],report['dmarc']['count']) ))
-    print('Total DMARC PASS:    %s   %.2f%%' % (report['dkim']['pass'],percentage(report['dkim']['pass'],report['dmarc']['count']) ))
+    print('Total DMARC FAIL:    %s   %.2f%%' % (report['dmarc']['fail'],percentage(report['dmarc']['fail'],report['dmarc']['count']) ))
+    print('Total DMARC PASS:    %s   %.2f%%' % (report['dmarc']['pass'],percentage(report['dmarc']['pass'],report['dmarc']['count']) ))
     print("")
     print('DMARC Policy None:               %s   %.2f%%' % (report['dmarc']['policy']['none'],percentage(report['dmarc']['policy']['none'],report['dmarc']['count']) ))
     print('DMARC Policy Quarantine:         %s   %.2f%%' % (report['dmarc']['policy']['quarantine'],percentage(report['dmarc']['policy']['quarantine'],report['dmarc']['count']) ))
@@ -172,17 +164,11 @@ def process_mailbox(M):
     print('DMARC Result Quarantine:         %s   %.2f%%' % (report['dmarc']['appliedPolicy']['quarantine'],percentage(report['dmarc']['appliedPolicy']['quarantine'],report['dmarc']['count']) ))
     print('DMARC Result Reject:             %s   %.2f%%' % (report['dmarc']['appliedPolicy']['reject'],percentage(report['dmarc']['appliedPolicy']['reject'],report['dmarc']['count']) ))
     
-    #formatted_json = json.dumps(report, indent=4)
-
-
-    #colorful_json = highlight(formatted_json, lexers.JsonLexer(), Terminal256Formatter(style=get_style_by_name('colorful')))
-    #print(colorful_json)
-
 
 M = imaplib.IMAP4_SSL('imap.gmail.com')
 
 try:
-    rv, data = M.login(EMAIL_ACCOUNT,  getpass.getpass())
+    rv, data = M.login(EMAIL_ACCOUNT, getpass.getpass())
 except imaplib.IMAP4.error:
     print ("LOGIN FAILED!!! ")
     sys.exit(1)
@@ -204,7 +190,3 @@ else:
     print("ERROR: Unable to open mailbox ", rv)
 
 M.logout()
-
-#myList = ["nsuok.edu", "cnn.com"]
-
-# checkdmarc.check_domains(myList)
